e2b50527ec5c28be3c83f359343dcdb9ab98fcb2
Suuma <30609754+suumani@users.noreply.github.com>
2025-02-21 18:17:19 +0900

    ver.0.2.1 all spawner

--- control.lua
+++ control.lua
@@ -3,17 +3,24 @@
 -- ----------------------------
 require("scripts.common.choose_quality")
 
+
+script.on_init(function()
+end)
+
 ----------------------------
 -- マップ生成時の巣とワームを置換
 -- ----------------------------
 script.on_event(defines.events.on_chunk_generated, function(event)
-	local spawners = event.surface.find_entities_filtered{area = event.area, name = {"biter-spawner", "spitter-spawner", "gleba-spawner-small", "gleba-spawner"}}
+
+	local spawners = nil
+	spawners = event.surface.find_entities_filtered{area = event.area, force = "enemy", type = "unit-spawner"}
+
 	for _, spawner in pairs(spawners) do
 		-- 品質処理
 		replace_common(spawner)
 	end
 
-	local worms = event.surface.find_entities_filtered{area = event.area, type = "turret", name = {"small-worm-turret", "medium-worm-turret", "big-worm-turret", "behemoth-worm-turret"}}
+	local worms = event.surface.find_entities_filtered{area = event.area, force = "enemy", type = "turret"}
 	for _, worm in pairs(worms) do
 		-- 品質処理
 		replace_common(worm)
@@ -37,10 +44,7 @@ function replace_common(entity)
 		entity.type == "unit" or 
 		entity.type == "spider-unit" or 
 		entity.type == "turret" or 
-		entity.name == "biter-spawner" or 
-		entity.name == "gleba-spawner" or 
-		entity.name == "gleba-spawner-corpse" or
-		entity.name == "gleba-spawner-corpse-small"
+		entity.type == "unit-spawner"
 	) and entity.force.name == "enemy" then
 		
 		-- 進化度の取得
@@ -62,71 +66,17 @@ end
 -- ----------------------------
 function replace_with_high_quality(old_entity, strquality)
 	
-	if(old_entity.type == "turret" 
-		and (
-			old_entity.quality ~= "legendary"
-			and old_entity.quality ~= "epic"
-			and old_entity.quality ~= "rare"
-			and old_entity.quality ~= "uncommon"
-			)
-		) then
-		-- ユニットの再配置 ワーム(ワームは破壊なし)
-		local result = place_warm(old_entity, strquality)
-		-- 設置後に破壊
-		if result == "success" then
-			old_entity.destroy()
-		end
-
-	else
-		-- ワーム以外は破壊
-		local name = old_entity.name
-		local surface = old_entity.surface
-		local position = old_entity.position
-		local force = old_entity.force
-		old_entity.destroy()
-		local new_entity = surface.create_entity{
-			name = name,
-			position = position,
-			force = force,
-			quality = strquality
-		}
-	end
-
-	-- debug
-	-- game.print(name.." quality = ".. strquality)
-
-end
-
-
--- ----------------------------
--- ユニットの再配置 ワーム(初期ワームは破壊)
--- ----------------------------
-function place_warm(old_entity, strquality)
-
 	local name = old_entity.name
 	local surface = old_entity.surface
-	
-	local min_distance = 3
-	local max_distance = 5
-	-- 設置可能座標を左上から探索
-    for dx = -max_distance, max_distance do
-	    for dy = -max_distance, max_distance do
-	    	local distance = dx*dx + dy*dy
-	    	if distance >= min_distance*min_distance and distance <= max_distance*max_distance then
-	    		local spawn_position = {x = old_entity.position.x + dx, y = old_entity.position.y + dy}
-				if surface.can_place_entity{name = name, position = spawn_position} then
-					local new_entity = surface.create_entity{
-						name = name,
-						position = spawn_position,
-						force = old_entity.force,
-						quality = strquality
-					}
-					return "success"
-				end
-	    	end
-    	end
-    end
-	
-	return "failed"
-	
+	local position = old_entity.position
+	local force = old_entity.force
+	old_entity.destroy()
+	local new_entity = surface.create_entity{
+		name = name,
+		position = position,
+		force = force,
+		quality = strquality
+	}
+
 end
+
